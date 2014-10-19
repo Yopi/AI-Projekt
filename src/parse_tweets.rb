6df20a6e2744849c16b444afc70d307a55bcc4b2
Jesper Bränn <brann.jesper@gmail.com>
2014-10-19 21:27:22 +0200

    Remove underlines from corpus§

--- src/parse_tweets.rb
+++ src/parse_tweets.rb
@@ -1,10 +1,11 @@
 require 'json'
-cb = JSON.parse(File.open('cb.json').read)
+cb = JSON.parse(File.open('../data/cb.json').read)
 
 URI_REGEX = %r"((?:(?:[^ :/?#]+):)(?://(?:[^ /?#]*))(?:[^ ?#]*)(?:\?(?:[^ #]*))?(?:#(?:[^ ]*))?)"
 cb['tweets'].map! do |tweet|
   tweet.gsub("\n\r", '')
 end
 tweets = cb['tweets'].join("\n")
+tweets.gsub!(/[_]/, '')
 tweets.gsub!(URI_REGEX, '')
 puts tweets
