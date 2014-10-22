c8832976a3eda97a3917a9d1f5f3f8071374391e
Daniel Duberg <dduberg@csc.kth.se>
2014-10-22 10:05:11 +0200

    Remove imports

--- src/GenerateTweet.java
+++ src/GenerateTweet.java
@@ -1,15 +1,3 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
